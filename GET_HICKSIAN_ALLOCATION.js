273736e8df926ee86fb1a8956a01e360ce40a883
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2021-12-10 12:45:21 -0500

    Delete GET_HICKSIAN_ALLOCATION.js

--- GET_HICKSIAN_ALLOCATION.js
+++
@@ -1,7 +0,0 @@
-function GET_HICKSIAN_ALLOCATION(OBJ, UTILITY) {
- return {
-  'x': UTILITY*(OBJ.py*OBJ.alpha/(OBJ.px*(1-OBJ.alpha)))**(1-OBJ.alpha),
-  'y': UTILITY*((OBJ.px/OBJ.py)*((1-OBJ.alpha)/OBJ.alpha))**(OBJ.alpha),
-  'e': OBJ.px*(UTILITY*(OBJ.py*OBJ.alpha/(OBJ.px*(1-OBJ.alpha)))**(1-OBJ.alpha)) + OBJ.py*(UTILITY*((OBJ.px/OBJ.py)*((1-OBJ.alpha)/OBJ.alpha))**(OBJ.alpha))
- };
-};
